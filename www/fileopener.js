992d916c8eba4093a0b078e6910cd9956922ec7e
MatseAdmin <MatseAdmin@167-062.eduroam.rwth-aachen.de>
2013-10-23 13:52:36 +0200

    fileopener.js fixed

--- www/fileopener.js
+++ www/fileopener.js
@@ -1,21 +1,18 @@
-var FileOpenerPlugin = function() {
-};
-
-FileOpenerPlugin.prototype.open = function (url, failureCB) {
-       	var success = function() {
-       		console.log("success!");
-       	}
-       	var failure = function(error) {
-      		console.log(error);
-		if(typeof failureCB === "function"){            
-			failureCB(error);
-	   	}
-        }
-	cordova.exec(success, failure, "FileOpener", "openFile", [url]);
+var fileopener = {
+	open:function (url, failureCB) {
+      	 	var success = function() {
+       			console.log("success!");
+       		}
+       		var failure = function(error) {
+      			console.log(error);
+			if(typeof failureCB === "function"){            
+				failureCB(error);
+	   		}
+		}
+		cordova.exec(success, failure, "FileOpener", "openFile", [url]);
+	}
 }
-
-var FileOpenerPlugin = new FileOpenerPlugin();
-module.exports = FileOpenerPlugin;
+module.exports = fileopener;
 
 
 
