d123e9f89ff255edc23a224be64d914b0db16268
TobiTh <tthieron@hotmail.de>
2013-11-12 14:16:53 +0100

    Update FileOpener.java

--- src/android/FileOpener.java
+++ src/android/FileOpener.java
@@ -56,65 +56,11 @@ public class FileOpener extends CordovaPlugin {
         // When the if condition is matched, plugin sets the correct intent (mime) type,
         // so Android knew what application to use to open the file
 
-        if (url.contains(".doc") || url.contains(".docx")) {
-            // Word document
-            intent = new Intent(Intent.ACTION_VIEW);
-            intent.setDataAndType(uri, "application/msword");
-        } else if(url.contains(".pdf")) {
-            // PDF file
-            intent = new Intent(Intent.ACTION_VIEW);
-            intent.setDataAndType(uri, "application/pdf");
-        } else if(url.contains(".ppt") || url.contains(".pptx")) {
-            // Powerpoint file
-            intent = new Intent(Intent.ACTION_VIEW);
-            intent.setDataAndType(uri, "application/vnd.ms-powerpoint");
-        } else if(url.contains(".xls") || url.contains(".xlsx")) {
-            // Excel file
-            intent = new Intent(Intent.ACTION_VIEW);
-            intent.setDataAndType(uri, "application/vnd.ms-excel");
-        } else if(url.contains(".rtf")) {
-            // RTF file
-            intent = new Intent(Intent.ACTION_VIEW);
-            intent.setDataAndType(uri, "application/rtf");
-        } else if(url.contains(".wav")) {
-            // WAV audio file
-            intent = new Intent(Intent.ACTION_VIEW);
-            intent.setDataAndType(uri, "audio/x-wav");
-        } else if(url.contains(".gif")) {
-            // GIF file
-            intent = new Intent(Intent.ACTION_VIEW);
-            intent.setDataAndType(uri, "image/gif");
-        } else if(url.contains(".jpg") || url.contains(".jpeg")) {
-            // JPG file
-            intent = new Intent(Intent.ACTION_VIEW);
-            intent.setDataAndType(uri, "image/jpeg");
-        } else if(url.contains(".txt")) {
-            // Text file
-            intent = new Intent(Intent.ACTION_VIEW);
-            intent.setDataAndType(uri, "text/plain");
-        } else if(url.contains(".mpg") || url.contains(".mpeg") || url.contains(".mpe") || url.contains(".mp4") || url.contains(".avi")) {
-            // Video files
-            intent = new Intent(Intent.ACTION_VIEW);
-            intent.setDataAndType(uri, "video/*");
-        }
-                
-        //if you want you can also define the intent type for any other file
         
-        //additionally use else clause below, to manage other unknown extensions
-        //in this case, Android will show all applications installed on the device
-        //so you can choose which application to use
+        String mimeType = URLConnection.guessContentTypeFromName(url);
+        intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(uri, mimeType);
         
-        // else {
-        // intent = new Intent(Intent.ACTION_VIEW);
-        // intent.setDataAndType(uri, "*/*");
-        // }
-
-        else {
-            String mimeType = URLConnection.guessContentTypeFromName(url);
-            intent = new Intent(Intent.ACTION_VIEW);
-            intent.setDataAndType(uri, mimeType);
-        }
-
         try
         {
               this.cordova.getActivity().startActivity(intent);
